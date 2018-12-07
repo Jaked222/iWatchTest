d0fa019d05489018094d010a7ec5caaf77800350
Jake Duncan <jakecduncan@gmail.com>
2018-12-07 12:01:06 -0500

    Initial starting point

---
+++ iWatchTest.OnWatchExtension/ModalInterfaceController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using WatchKit;
+
+namespace iWatchTest.OnWatchExtension
+{
+    public partial class ModalInterfaceController : WKInterfaceController
+    {
+        public ModalInterfaceController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
